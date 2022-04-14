e4dfb48e5f01e58e0d2b9fbc20e7f0c714b2b746
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-04-14 15:57:46 -0300

    buttons, modal delete

--- src/components/Forms/Main/FormMain.jsx
+++ src/components/Forms/Main/FormMain.jsx
@@ -23,12 +23,31 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 800,
+  width: 880,
+  height: 480,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4
+  p: 4,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
 }
+
+const ModalEdit = styled.div`
+  border: 1px solid;
+  width: 723px;
+  padding-bottom: 10px;
+  margin-bottom: 30px;
+`
+
+const ModalDelete = styled.div`
+  border: 1px solid;
+  width: 661px;
+  height: 168px;
+  padding-bottom: 10px;
+  margin-bottom: 30px;
+`
 
 const pointer = {
   cursor: 'pointer',
@@ -39,6 +58,20 @@ const DviButtons = styled.div`
   display: flex;
   justify-content: flex-end;
   flex-direction: row;
+  padding-right: 18px;
+`
+
+const Btn = styled.button`
+  border: 1px solid;
+  background-color: #fff;
+  color: black;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  height: 33px;
+  margin-top: 30px;
+  margin-right: 16px;
+  width: 111px;
 `
 
 function FormMain() {
@@ -92,7 +125,7 @@ function FormMain() {
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <StyleFormCreate>
+            <ModalEdit>
               <Subtitle subtitle="Edit" />
               <Label bottom="13px" label="Title" />
               <input
@@ -121,7 +154,7 @@ function FormMain() {
               >
                 SAVE
               </button>
-            </StyleFormCreate>
+            </ModalEdit>
           </Box>
         </Modal>
 
@@ -132,14 +165,14 @@ function FormMain() {
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <StyleFormCreate>
+            <ModalDelete>
               <Subtitle subtitle="Are you sure you want to delete this item?" />
               <DviButtons>
-                <button>Cancel</button>
+                <Btn onClick={handleCloseDelete}>Cancel</Btn>
 
-                <button>OK</button>
+                <Btn>OK</Btn>
               </DviButtons>
-            </StyleFormCreate>
+            </ModalDelete>
           </Box>
         </Modal>
         <Subtitle subtitle="What’s on your mind?" />
