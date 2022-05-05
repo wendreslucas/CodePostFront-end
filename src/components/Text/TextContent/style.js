ac07b4c327e2f8fcbca6ea527b56953c212c718e
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-05 10:39:33 -0300

    env

--- src/components/Text/TextContent/style.js
+++ src/components/Text/TextContent/style.js
@@ -7,12 +7,13 @@ export const StyleText = styled.p`
   color: white;
   font-size: 18px;
   font-weight: 400;
-  line-height: 21px;
+  line-height: 23px;
   padding: 0 31px 10px 30px;
   text-align: left;
   vertical-align: top;
 
   ${media.mobile} {
     font-size: 15px;
+    line-height: 20px;
   }
 `
