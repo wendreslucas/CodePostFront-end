e48b7ecc417bf416dbd556504b1d476040970d7c
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-06 17:06:45 -0300

    cursor...

--- src/components/Icons/Edit/style.js
+++ src/components/Icons/Edit/style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const Button = styled.button`
   border: none;
   background-color: transparent;
-  cursor: pointer;
 
   img {
     visibility: ${props => (props.disabled ? 'hidden' : 'visible')};
+    cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
   }
 `
